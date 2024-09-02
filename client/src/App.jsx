763d567a390d766be3e545eb606b1cf669c7f1db
Muhammmad-Hassan <hassankhan5uy99@gmail.com>
2024-09-02 03:27:45 -0700

    don merge this

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,28 +1,12 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import HeroSection from './components/HeroSection'; 
-import AboutPage from './pages/About.jsx';
-import ContactUsPage from './pages/ContactUsPage';
-import SignIn from './components/SignIn.jsx';
-import SignUp from './components/SignUp.jsx';
-
+import React from 'react'
+import Header from './assets/components/Header'
 
 function App() {
   return (
     <>
-      <Router>
-        {/* <Header />  */}
-        <Routes>
-          <Route path="/" element={<HeroSection />} />
-          <Route path="/about" element={<AboutPage />} />
-          <Route path="/contact" element={<ContactUsPage/>} />
-          <Route path="/SignIn" element={<SignIn />} />
-          <Route path="/SignUp" element={<SignUp />} />
-
-        </Routes>
-      </Router>
+    <Header/>
     </>
-  );
+  )
 }
 
-export default App;
+export default App
