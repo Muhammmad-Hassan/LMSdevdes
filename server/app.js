f5898671db489662044a93d48478397915ffc6e8
Muhammmad-Hassan <hassankhan5uy99@gmail.com>
2024-09-06 13:03:14 -0700

    addin

--- server/app.js
+++ server/app.js
@@ -27,32 +27,32 @@ app.get("/", (req, res) => {
 })
 
 
-app.post('/api/admin-register', async (req, res) => {
-    const { adminEmail, adminPassword } = req.body;
-    console.log(req.body);
+// app.post('/api/admin-register', async (req, res) => {
+//     const { adminEmail, adminPassword } = req.body;
+//     console.log(req.body);
 
-    try {
-        // Check if the admin already exists
-        const existingAdmin = await Admin.findOne({ adminEmail });
-        if (existingAdmin) {
-            return res.status(400).json({ message: 'Admin already exists' });
-        }
+//     try {
+//         // Check if the admin already exists
+//         const existingAdmin = await Admin.findOne({ adminEmail });
+//         if (existingAdmin) {
+//             return res.status(400).json({ message: 'Admin already exists' });
+//         }
 
-        // Hash the password before saving
-        const salt = await bcrypt.genSalt(10);
-        const hashedPassword = await bcrypt.hash(adminPassword, salt);
+//         // Hash the password before saving
+//         const salt = await bcrypt.genSalt(10);
+//         const hashedPassword = await bcrypt.hash(adminPassword, salt);
 
-        // Create a new admin
-        const admin = await Admin.create({ adminEmail, adminPassword: hashedPassword });
+//         // Create a new admin
+//         const admin = await Admin.create({ adminEmail, adminPassword: hashedPassword });
 
-        // Generate a token
-        console.log(admin);
-        res.status(201).json({ message: "Admin created successfully" });
-    } catch (error) {
-        console.error(error.message);
-        res.status(500).json({ message: 'Server error' });
-    }
-});
+//         // Generate a token
+//         console.log(admin);
+//         res.status(201).json({ message: "Admin created successfully" });
+//     } catch (error) {
+//         console.error(error.message);
+//         res.status(500).json({ message: 'Server error' });
+//     }
+// });
 
 
 
