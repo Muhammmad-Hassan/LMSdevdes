b430cdd07718017eb0f2c60453f0519368584cda
Muhammmad-Hassan <hassankhan5uy99@gmail.com>
2024-09-06 12:11:43 -0700

    remove

--- server/app.js
+++ server/app.js
@@ -4,8 +4,8 @@ const cors = require('cors');
 const app = express();
 const port = 5000;
 const path = require('path');
-const authRoutes = require("./routes/authRoutes")
-const profileRoutes = require('./routes/Profile'); // Import the profile route
+// const authRoutes = require("./routes/authRoutes")
+// const profileRoutes = require('./routes/Profile'); // Import the profile route
 require("./db/conn")
 // Middleware
 
@@ -18,12 +18,12 @@ app.use(express.json());
 app.use(express.static(path.join(__dirname, 'uploads'))); 
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
-app.use("/", (req, res) => {
+app.get("/", (req, res) => {
     res.json("Hello Khan");
 })
 
-app.use('/api/auth', authRoutes);
-app.use('/api', profileRoutes);
+// app.use('/api/auth', authRoutes);
+// app.use('/api', profileRoutes);
 
 
 
