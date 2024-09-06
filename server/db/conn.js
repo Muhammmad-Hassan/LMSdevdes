750844c5d779e36befc897deadb67d75f0001e2e
Muhammmad-Hassan <hassankhan5uy99@gmail.com>
2024-09-06 10:28:04 -0700

    change the url of data base

--- server/db/conn.js
+++ server/db/conn.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 
 
-mongoose.connect("mongodb://127.0.0.1:27017/lms").then(()=>{
+mongoose.connect("mongodb+srv://hassan:hassan123@cluster0.1uazb.mongodb.net/dev?retryWrites=true&w=majority&appName=Cluster0").then(()=>{
     console.log('connection Success!')
 }).catch((err)=>{
     console.log('connection faild', err)
