6065380a9b52d116fcdeb0a120e086cf1ef8a31f
Muhammmad-Hassan <hassankhan5uy99@gmail.com>
2024-09-06 12:28:32 -0700

    adding

--- server/app.js
+++ server/app.js
@@ -19,7 +19,7 @@ app.use(express.static(path.join(__dirname, 'uploads')));
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
 
-app.use('/api/auth', authRoutes);
+// app.use('/api/auth', authRoutes);
 app.use('/api', profileRoutes);
 
 app.get("/", (req, res) => {
