260d05ca61115b4fbb8ccda40f23b55d42097b59
Muhammmad-Hassan <hassankhan5uy99@gmail.com>
2024-09-06 11:10:32 -0700

    addinng again

--- server/db/conn.js
+++ server/db/conn.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 
 
-mongoose.connect("mongodb+srv://hassan:hassan123@cluster0.1uazb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0").then(()=>{
+mongoose.connect("mongodb+srv://hassan:hassan123@cluster0.1uazb.mongodb.net/lms?retryWrites=true&w=majority&appName=Cluster0").then(()=>{
     console.log('connection Success!')
 }).catch((err)=>{
     console.log('connection faild', err)
